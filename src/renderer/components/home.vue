a28d4763ba548c11fa350a40be65ee9808a15769
niuyueyang <3223891963@qq.com>
2019-05-04 16:59:56 +0800

    Update home.vue

--- src/renderer/components/home.vue
+++ src/renderer/components/home.vue
@@ -9,9 +9,27 @@
 
 <script>
   import Common from './common';
-  import {shell} from 'electron';
+  import {shell,remote} from 'electron';
     export default {
         name: "home",
+      mounted(){
+        const {Menu, MenuItem} = remote;
+        //右键餐单
+        const menu = new Menu();
+        menu.append(new MenuItem({
+          label: '放大',
+          click:function ()  {
+            console.log('item 1 clicked')
+          }
+        }));
+        menu.append(new MenuItem({type: 'separator'}));//分割线
+        menu.append(new MenuItem({label: '缩小', type: 'checkbox', checked: true}));//选中
+
+        window.addEventListener('contextmenu', (e) => {
+          e.preventDefault();
+          menu.popup({window: remote.getCurrentWindow()})
+        }, false)
+      },
       components: {Common},
       methods:{
             tag(){
